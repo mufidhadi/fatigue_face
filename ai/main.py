d7293818ab6c14bfa620308279ed1fc32d35bb16
mufidhadi <mufidmove@gmail.com>
2024-06-05 06:39:05 +0700

    add eye closed duration

--- ai/main.py
+++ ai/main.py
@@ -7,12 +7,28 @@ from ultralytics import YOLO
 import time
 import mysql.connector
 import mysql_default
+import argparse
 
-mydb = mysql.connector.connect(**mysql_default.mysql_config)
 
 yolo_model_path = './model/eye_best.onnx'
-video_source = './mufid2.mp4'
-counting_time = 1000 * 60  # 1 minute
+video_source = 0
+counting_time = 60  # 1 minute
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--video_source','-s', type=str, default=video_source)
+parser.add_argument('--yolo_model_path', '-m', type=str, default=yolo_model_path)
+parser.add_argument('--counting_time','-t', type=int, default=counting_time)
+args = parser.parse_args()
+
+yolo_model_path = 0 if args.yolo_model_path == '0' else args.yolo_model_path
+video_source = args.video_source
+counting_time = args.counting_time
+
+video_source = args.video_source
+yolo_model_path = args.yolo_model_path
+counting_time = args.counting_time
+
+mydb = mysql.connector.connect(**mysql_default.mysql_config)
 
 def data_uri_to_cv2_img(uri):
     encoded_data = uri.split(',')[1]
@@ -67,12 +83,16 @@ EYE_CLOSED = 1
 
 closed_eyes_count = 0
 last_eye_state = -1
+total_counting_time = 0
+eye_closed_duration = 0
 
 # Start capturing video
 print('start capturing...')
 cap = cv2.VideoCapture(video_source)
 
 start_time = time.time()
+abs_start_time = start_time
+eye_closed_time_start = start_time
 
 while True:
     # Capture frame-by-frame
@@ -113,31 +133,40 @@ while True:
             print('error save image')
 
         # Draw rectangle around face and label with predicted emotion
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 255), 1)
         if have_verified:
-            cv2.putText(frame, 'Driver: '+verified_name, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
+            cv2.putText(frame, 'Driver: '+verified_name, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 255, 255), 1)
             yolo_result = yolo_model.predict(source=face_roi, conf=0.5, imgsz=320)
             for result in yolo_result:
                 for box in result.boxes:
                     x1, y1, x2, y2 = box.xyxy[0]
                     x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
                     frame_x1, frame_y1, frame_x2, frame_y2 = x + x1, y + y1, x + x2, y + y2
-                    cv2.rectangle(frame, (frame_x1, frame_y1), (frame_x2, frame_y2), (0, 255, 0), 2)
+                    cv2.rectangle(frame, (frame_x1, frame_y1), (frame_x2, frame_y2), (0, 255, 0), 1)
                     box_class = int(box.cls[0])
-                    cv2.putText(frame, yolo_classes[box_class], (frame_x1, frame_y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+                    cv2.putText(frame, yolo_classes[box_class], (frame_x1, frame_y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 1)
                     if last_eye_state != box_class:
                         if box_class == EYE_CLOSED:
                             closed_eyes_count += 1
+                            eye_closed_time_start = time.time()
+                        else:
+                            curr_eye_closed_duration = time.time() - eye_closed_time_start
+                            eye_closed_duration += curr_eye_closed_duration
                         last_eye_state = box_class
-            cv2.putText(frame, 'closed eye count: '+str(closed_eyes_count), (5, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
+            cv2.putText(frame, 'closed eye count: '+str(closed_eyes_count), (5, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 255, 255), 1)
         else:
-            cv2.putText(frame, 'recognizing...', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
+            cv2.putText(frame, 'recognizing...', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 255, 255), 1)
 
     current_time = time.time()
     elapsed_time = current_time - start_time
+    total_counting_time = current_time - abs_start_time
+
+    cv2.putText(frame, 'total time (s): '+str(int(total_counting_time)), (5, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 255, 255), 1)
+    cv2.putText(frame, "eye closed duration (s): %.2f" % eye_closed_duration, (5, 75), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 255, 255), 1)
+
     if elapsed_time >= counting_time:
-        sql = "INSERT INTO driving_log (employee_id, count) VALUES (%s, %s)"
-        val = (verified_id, closed_eyes_count)
+        sql = "INSERT INTO driving_log (employee_id, eye_closed_count, eye_closed_duration) VALUES (%s, %s, %s)"
+        val = (verified_id, closed_eyes_count, eye_closed_duration)
         mycursor = mydb.cursor()
         mycursor.execute(sql, val)
         mydb.commit()
@@ -145,6 +174,7 @@ while True:
         mycursor.close()
         start_time = current_time
         closed_eyes_count = 0
+        eye_closed_duration = 0
 
     # Display the resulting frame
     cv2.imshow('Face Detection', frame)
